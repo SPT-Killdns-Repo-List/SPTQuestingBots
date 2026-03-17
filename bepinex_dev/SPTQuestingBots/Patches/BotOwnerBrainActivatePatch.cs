cc9994fae7fc28c3104bbd8bf2f4bc3ac76a4b5a
Kirill Danshin <kirill@danshin.xyz>
2026-03-17 16:38:15 +0400

    return stuck fix

--- bepinex_dev/SPTQuestingBots/Patches/BotOwnerBrainActivatePatch.cs
+++ bepinex_dev/SPTQuestingBots/Patches/BotOwnerBrainActivatePatch.cs
@@ -34,6 +34,9 @@ namespace SPTQuestingBots.Patches
             {
                 Controllers.BotRegistrationManager.MakeBotGroupHostileTowardAllBosses(__instance);
             }
+
+            // Fix for bots getting stuck in Standby when enemy PMC's are near them
+            __instance.StandBy.CanDoStandBy = false;
         }
 
         private static void registerBot(BotOwner __instance)
